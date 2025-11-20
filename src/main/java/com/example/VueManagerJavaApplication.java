f5a5457b9ee0e1374e570d16eeb83e869a02fcf6
hgt <2962933152@qq.com>
2025-11-20 11:39:36 +0800

    第八章-进度2-完成Mybatis-plus整合

--- src/main/java/com/example/VueManagerJavaApplication.java
+++ src/main/java/com/example/VueManagerJavaApplication.java
@@ -1,4 +1,4 @@
-package com.example.vue_manager_java;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
