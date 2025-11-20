9507a73a48331e1c189b505a9b2971672bcf7fe1
hgt <2962933152@qq.com>
2025-11-20 11:22:03 +0800

    第八章-进度1-创建Spring Boot项目

---
+++ src/main/java/com/example/vue_manager_java/VueManagerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vue_manager_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueManagerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueManagerJavaApplication.class, args);
+	}
+
+}
